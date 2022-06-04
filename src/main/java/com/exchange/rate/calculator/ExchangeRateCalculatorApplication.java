c47d69b33ad5af80efdb470a04b8286a86fcf9b4
tongnamuu <thban33@naver.com>
2022-06-05 01:45:11 +0900

    feat: Project Setup

---
+++ src/main/java/com/exchange/rate/calculator/ExchangeRateCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.rate.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exchange/rate/calculator/ExchangeRateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.rate.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
