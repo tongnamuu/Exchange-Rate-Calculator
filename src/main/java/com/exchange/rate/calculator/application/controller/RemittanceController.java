57bdf6f978b337c39fa5e6e869ea41fb0ed0ef73
tongnamuu <thban33@naver.com>
2022-06-05 16:06:15 +0900

    feat: Create Remittance

---
+++ src/main/java/com/exchange/rate/calculator/application/controller/RemittanceController.java
@@ -0,0 +1,7 @@
+package com.exchange.rate.calculator.application.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RemittanceController {
+}
---
+++ src/main/java/com/exchange/rate/calculator/domain/entity/Remittance.java
@@ -0,0 +1,15 @@
+package com.exchange.rate.calculator.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Remittance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+
+}
