22d15bcba291a30b8e4f5dea187342b942383e7c
tongnamuu <thban33@naver.com>
2022-06-12 16:54:12 +0900

    feat: Update Remittance Domain

--- src/main/java/com/exchange/rate/calculator/domain/entity/Remittance.java
+++ src/main/java/com/exchange/rate/calculator/domain/entity/Remittance.java
@@ -1,6 +1,10 @@
 package com.exchange.rate.calculator.domain.entity;
 
+import java.time.ZonedDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,7 +13,18 @@ import javax.persistence.Id;
 public class Remittance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
 
+    @Column(name = "amount", nullable = false)
+    private Long amount;
+
+    @Column(name = "currency", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Currency currency;
+
+    @Column(name = "created_at", nullable = false)
+    private ZonedDateTime createdAt;
 }
--- src/main/java/com/exchange/rate/calculator/domain/repository/RemittanceRepository.java
+++ src/main/java/com/exchange/rate/calculator/domain/repository/RemittanceRepository.java
@@ -1,4 +1,7 @@
 package com.exchange.rate.calculator.domain.repository;
 
+import com.exchange.rate.calculator.domain.entity.Remittance;
+
 public interface RemittanceRepository {
+    Remittance save(Remittance remittance);
 }
