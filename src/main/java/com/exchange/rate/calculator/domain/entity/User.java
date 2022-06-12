f106ca3cbf300296b2ea713d9980fed6d6909ae8
tongnamuu <thban33@naver.com>
2022-06-12 17:39:11 +0900

    feat: Update PasswordEncoder

--- src/main/java/com/exchange/rate/calculator/domain/entity/User.java
+++ src/main/java/com/exchange/rate/calculator/domain/entity/User.java
@@ -8,11 +8,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,13 +28,34 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "name", nullable = false)
+    private String name;
+
     @Column(name = "last_login_datetime")
     private ZonedDateTime lastLoginDateTime;
 
+    @Builder
+    private User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    private static User create(
+        String email,
+        String encodedPassword,
+        String name
+    ) {
+        return User.builder()
+            .email(email)
+            .password(encodedPassword)
+            .name(name)
+            .build();
+    }
 
 
     public boolean verifyPassword(PasswordEncoder passwordEncoder, String rawPassword) {
-        return passwordEncoder.decode(this.password, rawPassword);
+        return passwordEncoder.matches(this.password, rawPassword);
     }
 
     public User login(PasswordEncoder passwordEncoder, String rawPassword) {
