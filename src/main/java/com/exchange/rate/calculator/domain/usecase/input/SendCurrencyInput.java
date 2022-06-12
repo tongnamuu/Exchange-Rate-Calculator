5981602d2d7b545289c49e5891effc3c1472dcdc
tongnamuu <thban33@naver.com>
2022-06-12 17:31:01 +0900

    fix: Change to double

--- src/main/java/com/exchange/rate/calculator/domain/usecase/input/SendCurrencyInput.java
+++ src/main/java/com/exchange/rate/calculator/domain/usecase/input/SendCurrencyInput.java
@@ -3,12 +3,14 @@ package com.exchange.rate.calculator.domain.usecase.input;
 import com.exchange.rate.calculator.domain.entity.Currency;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
+@Getter
 public class SendCurrencyInput implements Input {
     private final long userId;
-    private final long amount;
+    private final double amount;
     private final Currency currency;
 }
